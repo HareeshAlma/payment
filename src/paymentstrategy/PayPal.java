7603803616b28475ba274141671e2d7b7f3a8e99
Hari <Hareesh.alma@gmail.com>
2017-10-17 13:59:25 -0500

    sc - 498 - task 3

---
+++ src/paymentstrategy/PayPal.java
@@ -0,0 +1,10 @@
+package com.ah.paymentstrategy;
+
+public class PayPal implements PaymentInterface {
+
+	public boolean processpayment() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
